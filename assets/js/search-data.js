a73a7e40c2db13d3591cacefe07a89dd94b1ca5f
Attention-is-All-I-Need <78871780+Attention-is-All-I-Need@users.noreply.github.com>
2026-01-22 10:59:49 +0000

    Deploying to gh-pages from @ Attention-is-All-I-Need/Attention-is-All-I-Need.github.io@e745bdf64d4b8131836f7edaf4c8cf901d196a2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,8 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-started-a-research-internship-at-alibaba-group-enhancing-the-reasoning-capabilities-of-qwen3-models-through-data-synthesis-techniques",
-          title: 'Started a research internship at Alibaba Group, enhancing the reasoning capabilities of Qwen3...',
+            },},{id: "news-started-a-research-internship-at-alibaba-group-foundation-model-training-team-future-living-lab-enhancing-the-reasoning-capabilities-of-qwen3-models-through-data-synthesis-techniques",
+          title: 'Started a research internship at Alibaba Group (Foundation Model Training Team, Future Living...',
           description: "",
           section: "News",},{id: "news-completed-the-m-eng-in-computer-technology-at-zhejiang-university",
           title: 'Completed the M.Eng. in Computer Technology at Zhejiang University.',
